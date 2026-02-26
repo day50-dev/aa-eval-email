7ac115a17ad179b72582e1437044819204d852e9
Chris McKenzie <kristopolous@yahoo.com>
2026-02-25 22:50:31 -0800

    stuff

---
+++ art-analysis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+curl -s 'https://artificialanalysis.ai/leaderboards/models?is_open_weights=open_source&size_class=all' | grep -Po  '(?<=self.__next_f.push\(\[1,).*?(?=\]\))' \
+    | grep coding_inde \
+    | sed 's/^".../"/g' | head -1 \
+    | jq -r 'fromjson | .[3].children[0][3].models.[] | "\(.coding_index) \(.size_class) \(.name)"' \
+    | sort -n | sed 's/ /\t/;s/ /\t/' > current
+
+diff current old > change
+if [[ -s change ]] ; then
+  echo "Sending email"
+diff current old | awk ' { print $4 }' | tr '\n' ' ' > subj
+{
+cat << ENDL
+MIME-Version: 1.0
+Content-Type: multipart/alternative;
+ boundary="who-cares/nomatter"
+From: New Model! <chris@9ol.es>
+Subject: $(cat subj)
+
+This is a MIME-encapsulated message
+--who-cares/nomatter
+Content-Type: text/plain
+$(cat change)
+--who-cares/nomatter--
+
+ENDL
+} | /usr/bin/msmtp aa-new-model@googlegroups.com
+else
+  echo "No email"
+fi
+cp current old
+
